f745c6e212a3991fbb6ea14ae3ce27c7bd3facbb
Phillip <renaudtp@oregonstate.edu>
2021-09-16 21:33:01 -0500

    Update CameraController.cs
    Ability to Drag position of camera with mouse, zooming in and out, and rotation of the camera.

--- Scripts/CameraController.cs
+++ Scripts/CameraController.cs
@@ -4,25 +4,83 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public Transform camerTransform;
+
+    public float normalSpeed;
+    public float fastSpeed;
     public float movementSpeed;
     public float movementTime;
+    public float rotationAmount;
+    public Vector3 zoomAmount;
 
     public Vector3 newPosition;
+    public Quaternion newRotation;
+    public Vector3 newZoom;
+
+    public Vector3 dragStartPosition;
+    public Vector3 dragCurrentPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         newPosition = transform.position;
+        newRotation = transform.rotation;
+        newZoom = camerTransform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
+        HandleMouseInput();
         HandleMovenmentInput();
+    }
+
+    void HandleMouseInput()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float entry;
+
+            if(plane.Raycast(ray, out entry))
+            {
+                dragStartPosition = ray.GetPoint(entry);
+            }
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float entry;
+
+            if (plane.Raycast(ray, out entry))
+            {
+                dragCurrentPosition = ray.GetPoint(entry);
+
+                newPosition = transform.position + dragStartPosition - dragCurrentPosition;
+            }
+        }
     }
 
     void HandleMovenmentInput()
     {
+        // Camera Movement Speed
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            movementSpeed = fastSpeed;
+        }
+        else
+        {
+            movementSpeed = normalSpeed;
+        }
+
+        // Movement Keys
         if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             newPosition += (transform.forward * movementSpeed);
@@ -38,13 +96,35 @@ public class CameraController : MonoBehaviour
             newPosition += (transform.right * -movementSpeed);
         }
 
-
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             newPosition += (transform.right * movementSpeed);
         }
 
+        // Rotate Camera
+        if(Input.GetKey(KeyCode.Q))
+        {
+            newRotation *= Quaternion.Euler(Vector3.up * rotationAmount);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            newRotation *= Quaternion.Euler(Vector3.up * -rotationAmount);
+        }
+
+        // Zoom Camera In/Out
+        if(Input.GetKey(KeyCode.R))
+        {
+            newZoom += zoomAmount;
+        }
+
+        if (Input.GetKey(KeyCode.F))
+        {
+            newZoom -= zoomAmount;
+        }
 
         transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * movementTime);
+        camerTransform.localPosition = Vector3.Lerp(camerTransform.localPosition, newZoom, Time.deltaTime * movementTime);
     }
 }
